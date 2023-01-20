374db1c6106e438f6fde948819e364bcbce9189c
vggog <gluhovkich@gmail.com>
2023-01-20 18:56:04 +0500

    Add User model

---
+++ src/app/user/models.py
@@ -0,0 +1,24 @@
+import sqlalchemy as sa
+
+from src.core.base.models import BaseModel
+
+
+class UserModel(BaseModel):
+    __tablename__ = 'users'
+
+    username = sa.Column(
+        sa.String,
+        unique=True,
+    )
+    name = sa.Column(
+        sa.String,
+    )
+    surname = sa.Column(
+        sa.String,
+    )
+    email = sa.Column(
+        sa.String,
+    )
+    phone_number = sa.Column(
+        sa.Integer,
+    )
---
+++ src/core/base/models.py
@@ -0,0 +1,21 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import as_declarative
+
+from src.core.base.utils import get_current_datetime
+
+
+@as_declarative()
+class BaseModel:
+    id = sa.Column(
+        sa.Integer,
+        primary_key=True,
+    )
+    created_at = sa.Column(
+        sa.DateTime,
+        default=get_current_datetime(),
+    )
+    updated_at = sa.Column(
+        sa.DateTime,
+        default=get_current_datetime(),
+        onupdate=get_current_datetime(),
+    )
