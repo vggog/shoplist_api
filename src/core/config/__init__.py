8ef40ecad4cf2219d0cab868be51028ecfcef221
vggog <gluhovkich@gmail.com>
2023-01-25 00:10:40 +0500

    Add config administartion
    Forgot to add this files to commit fe3ba943445e73d7be25775007494a3cbc880e2d
    Therefore, I have to add a separate commit.

---
+++ src/core/config/__init__.py
@@ -0,0 +1,3 @@
+from src.core.config.settings import Config
+
+config = Config()
---
+++ src/core/config/settings.py
@@ -0,0 +1,25 @@
+import environ
+
+
+env = environ.Env()
+environ.Env.read_env()
+
+
+class Config:
+
+    # DB SETTINGS
+    POSTGRES_HOST: str = env('POSTGRES_HOST')
+    POSTGRES_USER_NAME: str = env('POSTGRES_USER')
+    POSTGRES_USER_PASSWORD: str = env('POSTGRES_PASSWORD')
+    POSTGRES_DB: str = env('POSTGRES_DB')
+    POSTGRES_PORT: int = env('POSTGRES_PORT')
+
+    # DB url for sqlalchemy
+    DB_URL = str(
+        f'postgresql+psycopg2://{POSTGRES_USER_NAME}:{POSTGRES_USER_PASSWORD}'
+        f'@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
+    )
+
+    # Redis settings
+    REDIS_HOST: str = env('REDIS_HOST')
+    REDIS_PORT: int = env('REDIS_PORT')
